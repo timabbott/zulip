24ad9143fe574c801a1698ac2802c427f655a2cf
Greg Price <greg@zulipchat.com>
2017-10-03 16:53:40 -0700

    debug: Add facility to dump tracemalloc snapshots.

--- zerver/lib/debug.py
+++ zerver/lib/debug.py
@@ -1,10 +1,17 @@
 
 import code
+from datetime import datetime
+import logging
+import os.path
 import signal
 import traceback
+import tracemalloc
 from types import FrameType
 
+from django.conf import settings
 from typing import Optional
+
+logger = logging.getLogger('zulip.debug')
 
 # Interactive debugging code from
 # http://stackoverflow.com/questions/132058/showing-the-stack-trace-from-a-running-python-application
@@ -30,3 +37,24 @@ def interactive_debug_listen():
     # type: () -> None
     signal.signal(signal.SIGUSR1, lambda sig, stack: traceback.print_stack(stack))
     signal.signal(signal.SIGUSR2, interactive_debug)
+
+def tracemalloc_dump(sig, frame):
+    # type: (int, FrameType) -> None
+    if not tracemalloc.is_tracing():
+        logger.warn("tracemalloc dump: tracing off, nothing to dump")
+        return
+    basename = "snap.{}.{}".format(os.getpid(),
+                                   datetime.utcnow().strftime("%F-%T"))
+    path = os.path.join(settings.TRACEMALLOC_DUMP_DIR, basename)
+    os.makedirs(settings.TRACEMALLOC_DUMP_DIR, exist_ok=True)
+    tracemalloc.take_snapshot().dump(path)
+    logger.info("tracemalloc dump: dumped {}".format(basename))
+
+def tracemalloc_listen():
+    # type: () -> None
+    '''Useful only when tracemalloc tracing enabled.
+
+    See https://docs.python.org/3/library/tracemalloc .
+    '''
+    assert(signal.SIGRTMIN < signal.SIGRTMAX)
+    signal.signal(signal.SIGRTMIN, tracemalloc_dump)
