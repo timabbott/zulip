4a569f6f31de72032a146d1f3ace8bf3cafaaf31
Priyank Patel <priyank3219@gmail.com>
2018-02-06 01:16:24 +0000

    install-node: Upgrade node to v8.5.0 and yarn to v1.4.1.
    This commit also upgrades nvm to v0.33.8.

--- frontend_tests/node_tests/timerender.js
+++ frontend_tests/node_tests/timerender.js
@@ -195,7 +195,7 @@ zrequire('timerender');
 
 (function test_set_full_datetime() {
     var message = {
-        timestamp: 1495091573, // 5/18/2017 7:12:53 AM (UTC+0)
+        timestamp: 1495091573, // 5/18/2017 7:12:53 (UTC+0)
     };
     var time_element = $('<span/>');
     var attrs = new Dict();
@@ -205,7 +205,12 @@ zrequire('timerender');
         return time_element;
     };
 
-    var expected = '5/18/2017 7:12:53 AM (UTC+0)';
+    // in node >= 8.5.0 date.toLocaleDateString and
+    // date.toLocaleTimeString are change instead of
+    // 5/18/2017 7:12:53 AM (UTC+0) they now return
+    // 2017-5-18 07:12:53 (UTC+0)
+    var time = new Date(message.timestamp * 1000);
+    var expected = `${time.toLocaleDateString()} 07:12:53 (UTC+0)`;
     timerender.set_full_datetime(message, time_element);
     var actual = attrs.get('title');
     assert.equal(expected, actual);
