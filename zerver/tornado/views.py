f8349c667e745a3c66b30376a8b2c403b87390d0
Tim Abbott <tabbott@zulipchat.com>
2018-07-13 16:28:16 +0530

    api: Stop using API keys for Django->Tornado authentication.
    As part of our effort to change the data model away from each user
    having a single API key, we're eliminating the couple requests that
    were made from Django to Tornado (as part of a /register or home
    request) where we used the user's API key grabbed from the database
    for authentication.
    
    Instead, we use the (already existing) internal_notify_view
    authentication mechanism, which uses the SHARED_SECRET setting for
    security, for these requests, and just fetch the user object using
    get_user_profile_by_id directly.

--- zerver/tornado/views.py
+++ zerver/tornado/views.py
@@ -9,10 +9,10 @@ from django.utils.translation import ugettext as _
 
 from zerver.decorator import REQ, RespondAsynchronously, \
     _RespondAsynchronously, asynchronous, \
-    has_request_variables, internal_notify_view
+    has_request_variables, internal_notify_view, process_client
 from zerver.lib.response import json_error, json_success
 from zerver.lib.validator import check_bool, check_list, check_string
-from zerver.models import Client, UserProfile, get_client
+from zerver.models import Client, UserProfile, get_client, get_user_profile_by_id
 from zerver.tornado.event_queue import fetch_events, \
     get_client_descriptor, process_notification
 from zerver.tornado.exceptions import BadEventQueueIdError
@@ -33,6 +33,16 @@ def cleanup_event_queue(request: HttpRequest, user_profile: UserProfile,
     request._log_data['extra'] = "[%s]" % (queue_id,)
     client.cleanup()
     return json_success()
+
+@asynchronous
+@internal_notify_view(True)
+@has_request_variables
+def get_events_internal(request: HttpRequest, handler: BaseHandler,
+                        user_profile_id: int=REQ()):
+    user_profile = get_user_profile_by_id(user_profile_id)
+    request._email = user_profile.email
+    process_client(request, user_profile, client_name="internal")
+    return get_events_backend(request, user_profile, handler)
 
 @asynchronous
 def get_events(request: HttpRequest, user_profile: UserProfile, handler: BaseHandler):
