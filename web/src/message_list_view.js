291e51dd8c2f1b8bbb1cefb71abb67d13b091f07
Tim Abbott <tabbott@zulip.com>
2023-05-28 22:22:28 -0700

    Revert "message_list_view: Minor optimization."
    This reverts commit 46a4964633ec0eb94bb47b519555197e64d9807a.

--- web/src/message_list_view.js
+++ web/src/message_list_view.js
@@ -1160,7 +1160,9 @@ export class MessageListView {
 
     set_message_offset(offset) {
         const $msg = this.selected_row();
-        message_viewport.scrollTop($msg.offset().top - offset);
+        message_viewport.scrollTop(
+            message_viewport.scrollTop() + $msg.get_offset_to_window().top - offset,
+        );
     }
 
     rerender_with_target_scrolltop(selected_row, target_offset) {
