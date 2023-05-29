c06adcb87786c10612261ffb3c7002ec85130fb0
Tim Abbott <tabbott@zulip.com>
2023-05-28 22:22:09 -0700

    Revert "overlays: Stop document from occupying the extra scrollbar width."
    This reverts commit 8654f839b5bdd19198b1ef0a6c50dc121b20ea10.

--- web/src/overlays.ts
+++ web/src/overlays.ts
@@ -50,17 +50,11 @@ function call_hooks(func_list: Hook[]): void {
 }
 
 export function disable_scrolling(): void {
-    // Why disable scrolling?
-    // Since fixed / absolute positined elements don't capture the scroll event unless
-    // they overflow their defined container. Since fixed / absolute elements are not treated
-    // as part of the document flow, we cannot capture `scroll` events on them and prevent propagation
-    // as event bubbling doesn't work naturally.
-    const scrollbar_width = window.innerWidth - document.documentElement.clientWidth;
-    $("html").css({"overflow-y": "hidden", "--disabled-scrollbar-width": `${scrollbar_width}px`});
+    $("html").css({"overflow-y": "hidden"});
 }
 
 function enable_scrolling(): void {
-    $("html").css({"overflow-y": "scroll", "--disabled-scrollbar-width": "0px"});
+    $("html").css({"overflow-y": "scroll"});
 }
 
 export function is_active(): boolean {
